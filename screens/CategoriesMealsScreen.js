9006855f34614189a2f00e432383bda57d60d62a
Anirudh Madhavan <anirudhragavender@gmail.com>
2021-05-24 00:29:19 +0530

    navigation init

---
+++ screens/CategoriesMealsScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {View,Text,StyleSheet, Button} from 'react-native'
+const CategoriesMealsScreen = (props) => {
+    return (
+        <View style={styles.screen}>
+            <Text>The Categories Meals Screen!</Text>
+            <Button title="Go to Details" onPress={()=>{
+                props.navigation.navigate('MealDetail')
+            }}/>
+            <Button title="Go Back" onPress={()=>{
+                props.navigation.goBack();
+            }}/>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    screen:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
+export default CategoriesMealsScreen
