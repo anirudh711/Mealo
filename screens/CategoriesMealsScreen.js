5fe5d697960a0a2e337a99436d9f43adab20b7c8
Anirudh Madhavan <anirudhragavender@gmail.com>
2021-05-27 14:21:23 +0530

    package updates and meal detail screen

--- screens/CategoriesMealsScreen.js
+++ screens/CategoriesMealsScreen.js
@@ -11,7 +11,11 @@ const CategoriesMealsScreen = (props) => {
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
         image={itemData.item.imageUrl}
-        onSelectMeal={() => {}}
+        onSelectMeal={() => {
+            props.navigation.navigate({routeName:'MealDetail',params:{
+                mealId:itemData.item.id
+            }})
+        }}
       />
     );
   };
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import {View,Text,StyleSheet} from 'react-native'
-const MealDetailScreen = () => {
+import {HeaderButtons,Item} from 'react-navigation-header-buttons';
+import {MEALS} from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton'
+const MealDetailScreen = (props) => {
+    const mealId = props.navigation.getParam('mealId')
+    const selectedMeal =MEALS.find(meal=>meal.id===mealId)
     return (
         <View style={styles.screen}>
-            <Text>The Meal Details Screen!</Text>
+            <Text>{selectedMeal.title}</Text>
         </View>
     )
 }
 
+MealDetailScreen.navigationOptions =(navigationData)=>{
+    const mealId = navigationData.navigation.getParam('mealId')
+    const selectedMeal =MEALS.find(meal=>meal.id===mealId)
+    return {
+        headerTitle: selectedMeal.title,
+        headerRight: ()=><HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title='Favorite' iconName='ios-star' onPress={()=>{
+                console.log('mark as fav!')
+            }}>
+
+            </Item>
+        </HeaderButtons>
+    }
+}
 const styles=StyleSheet.create({
     screen:{
         flex:1,
