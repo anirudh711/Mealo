d8cf4b49668fe64917d326f9ef4ce5fba955e659
Anirudh Madhavan <anirudhragavender@gmail.com>
2021-05-28 13:44:30 +0530

    intracomponent comm-FilterScreen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,38 +1,112 @@
-import React from 'react'
-import {View,Text,StyleSheet} from 'react-native';
+import React, { useState, useEffect, useCallback } from "react";
+import { View, Text, StyleSheet, Switch } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
-import COLORS from '../constants/colors'
- const FiltersScreen = () => {
-    return (
-        <View style={styles.screen}>
-            <Text>The Meal Details Screen!</Text>
-        </View>
-    )
-}
+import COLORS from "../constants/colors";
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: COLORS.primaryColor }}
+        thumbColor={COLORS.primaryColor}
+        value={props.state}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
+const FiltersScreen = (props) => {
+  const { navigation } = props;
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVeg, setIsVeg] = useState(false);
+
+  const saveFilters = useCallback(() => {
+    //this recreates the function only if these 4 states change, and caches it.
+    const appliedFilters = {
+      gf: isGlutenFree,
+      lf: isGlutenFree,
+      v: isVegan,
+      veg: isVegan,
+    };
+    console.log(appliedFilters);
+  }, [isGlutenFree, isGlutenFree, isVegan, isVeg]);
+  useEffect(() => {
+    navigation.setParams({ save: saveFilters }); //to update the params currently loaded in this screen so that the header right can access this
+  }, [saveFilters]);
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Available Filters/Restrictions</Text>
+      <FilterSwitch
+        label={"Gluten Free"}
+        state={isGlutenFree}
+        onChange={() => setIsGlutenFree((v) => !v)}
+      />
+      <FilterSwitch
+        label={"Lactose Free"}
+        state={isLactoseFree}
+        onChange={() => setIsLactoseFree((v) => !v)}
+      />
+      <FilterSwitch
+        label={"Vegan"}
+        state={isVegan}
+        onChange={() => setIsVegan((v) => !v)}
+      />
+      <FilterSwitch
+        label={"Vegetarian"}
+        state={isVeg}
+        onChange={() => setIsVeg((v) => !v)}
+      />
+    </View>
+  );
+};
 
 FiltersScreen.navigationOptions = (navData) => {
-    return {
-      headerTitle: "Filters",
-      headerLeft: () => (
-        <HeaderButtons HeaderButtonComponent={HeaderButton}>
-          <Item
-            title="Filters"
-            iconName="ios-menu"
-            onPress={() => {
-              navData.navigation.toggleDrawer();
-            }}
-          ></Item>
-        </HeaderButtons>
-      ),
-    };
+  return {
+    headerTitle: "Filters",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Filters"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        ></Item>
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Save"
+          iconName="ios-save"
+          onPress={navData.navigation.getParam("save")}
+        ></Item>
+      </HeaderButtons>
+    ),
   };
-const styles=StyleSheet.create({
-    screen:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 15,
+  },
+});
 
-export default FiltersScreen
+export default FiltersScreen;
